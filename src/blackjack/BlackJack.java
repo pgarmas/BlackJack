3277db04205611d788924695cc03b9d2334da52f
polina.garmas <polina.garmas@STUDENT-7>
2016-12-28 20:50:24 +0200

    игра готова без интерфейса

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -20,7 +20,7 @@ public class BlackJack {
      */
     public static void main(String[] args) {
         // TODO code application logic here,
-        
+        Scanner sn = new Scanner(System.in);
         Deck koloda = new Deck();
         koloda.shuffle();
         
@@ -31,17 +31,92 @@ public class BlackJack {
         dealerHand.add(koloda.getCard());
         playerHand.add(koloda.getCard());
         dealerHand.add(koloda.getCard());
+        
+        boolean gameOver = false;
+        boolean playerIsDone = false;
+       
+        while(!gameOver)
+        {
+            showOutcome(playerHand, dealerHand);
 
-        //пора решать
-        showOutcome(playerHand, dealerHand);
-        //System.out.println(koloda.getCard().name());
-        Scanner sn = new Scanner(System.in);
-        System.out.println("Сдать еще карту?");
-        String answer= sn.next();
-        if (answer.contains("y")) {
-            playerHand.add(koloda.getCard());
+            //пора решать
+            //проверка не перебор
+            //проверяем игрока
+            if (handValue(playerHand).get(0)>21) 
+            {
+                if (handValue(dealerHand).get(0)>21) {
+                   System.out.println("У обоих перебор, никто не выиграл!"); 
+                } else System.out.println("У игрока перебор, выиграл дилер!");
+                gameOver=true;
+            }else if (handValue(dealerHand).get(0)>21) 
+            {
+                System.out.println("У дилера перебор, выиграл игрок!");
+                gameOver=true;
+            }else if (has21(handValue(dealerHand)))
+            {
+                System.out.println("У дилера 21. Дилер выиграл!");
+                gameOver=true;
+            } else if (has21(handValue(playerHand)))
+            {
+                if (dealerNeedMoreCard(handValue(dealerHand))) {
+                    dealerHand.add(koloda.getCard());
+                    System.out.println("Дилер берет себе карту");
+                } else {
+                    System.out.println("У игрока 21. Игрок выиграл!");
+                    gameOver=true;
+                }
+            } else //ни у кого нет 21 и перебора
+            {
+                System.out.println("Сдать еще карту?");
+                String answer= sn.next();
+                if (answer.contains("y")) {
+                    playerHand.add(koloda.getCard());
+                    System.out.println("Дилер сдает игроку карту");
+                }else {
+                    playerIsDone=true;
+                }
+                if (dealerNeedMoreCard(handValue(dealerHand))) {
+                    dealerHand.add(koloda.getCard());
+                    System.out.println("Дилер берет себе карту");
+                } else if (playerIsDone){
+                    //все не хотят больше брать карт
+                    int playerBest = BestValue(handValue(playerHand));
+                    int dealerBest = BestValue(handValue(dealerHand));
+                    System.out.println("У игрока "+playerBest+" очков, у дилера "+dealerBest+" очков.");
+                    if (playerBest>dealerBest) {
+                        System.out.println("Игрок выиграл!");
+                    } else  if (dealerBest>playerBest){
+                        System.out.println("Дилер выиграл!");
+                    } else System.out.println("Ничья!");
+                    gameOver=true;
+                }
+            }
         }
-        showOutcome(playerHand, dealerHand);
+        System.out.println("Игра окончена.");
+    }
+    
+    
+    private static int BestValue (ArrayList<Integer> currentPoints) 
+    {
+        int bestValue=0;
+        for (Integer currentPoint : currentPoints) {
+            if (currentPoint>bestValue&&currentPoint<=21) {
+                bestValue=currentPoint;
+            }
+        }
+        return bestValue;
+    }
+   
+    private static boolean has21 (ArrayList<Integer> currentPoints){
+        return currentPoints.contains(21);
+    }
+    
+    private static boolean dealerNeedMoreCard (ArrayList<Integer> currentPoints){
+        return !(currentPoints.contains(17)||
+                currentPoints.contains(18)||
+                currentPoints.contains(19)||
+                currentPoints.contains(20)||
+                currentPoints.contains(21));
     }
     
     static void showOutcome(ArrayList<Card> playerHand,ArrayList<Card> dealerHand)
@@ -50,17 +125,17 @@ public class BlackJack {
          for (Card deck1 : playerHand) {
              System.out.println(deck1.name());
         }
-        System.out.println("Сумма карт: "+ handMinValue(playerHand)); 
+        System.out.println("Сумма карт: "+ handValue(playerHand)); 
         System.out.println("У дилера на руках:");
              for (Card deck1 : dealerHand) {
              System.out.println(deck1.name());
         }
-        System.out.println("Сумма карт: "+ handMinValue(dealerHand));    
+        System.out.println("Сумма карт: "+ handValue(dealerHand));    
 
     }
     
     
-    public static ArrayList<Integer> handMinValue (ArrayList<Card> hand)
+    public static ArrayList<Integer> handValue (ArrayList<Card> hand)
     {
         int sum=0;
         for (Card crd : hand) {
@@ -97,7 +172,13 @@ public class BlackJack {
             //add card value to the sum
             if (crd.getValue()==Values.Ace) numOfAces+=1;
             }
+        
         return numOfAces;
     }
+        
+       // модификаторДоступа типВозращаемогоЗначения имяМетода(список_параметров) - параметры - это получаемые значения 
+
+
+
     
 }
